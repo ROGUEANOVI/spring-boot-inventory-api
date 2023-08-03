012013819fb79a3e86122dec66f9f8e36f2c05de
ROGUEANOVI <ovidioromero66@gmail.com>
2023-08-03 17:22:33 -0500

    Add CORS

--- src/main/java/com/rogueanovi/inventory/controller/CategoryController.java
+++ src/main/java/com/rogueanovi/inventory/controller/CategoryController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/v1/category")
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class CategoryController {
     @Autowired
     private ICategoryService categoryService;
